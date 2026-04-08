2a49d38a89215dead7fcaebdab4951f44013acae
gtpesantren22 <cakromat26@gmail.com>
2026-04-08 10:12:35 +0700

    update: add qr location

--- application/views/absensi/hasil_kbm_siswa.php
+++ application/views/absensi/hasil_kbm_siswa.php
@@ -59,7 +59,7 @@
                                       AND id_lembaga = '$lmb->id_lembaga'
                                 ");
                                 ?>
-                                    <tr class="border-b border-slate-200 dark:border-slate-700 hover:bg-slate-50 dark:hover:bg-slate-800 transition">
+                                    <tr class="border-b border-slate-200 dark:border-slate-700 hover:bg-slate-50 dark:hover:bg-slate-800 transition" onclick="window.location.href='<?= site_url('kbm/absensi') ?>'">
                                         <!-- Jam -->
                                         <td class="px-3 py-2 text-slate-600 dark:text-slate-300 whitespace-nowrap">
                                             <?= $hasil->jam_dari . ' - ' . $hasil->jam_sampai ?>
